3ac2067e8215e480e36c8f81dcf9b0df92a0890b
Cameron Asbury <asbury56@live.marshall.edu>
2016-09-23 11:50:22 -0400

    Assignment 3 Start

---
+++ IST303_Asbury_C_A3/IST303_Asbury_C_A3/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST303_Asbury_C_A3
+{
+    class Player
+    {
+        private string name;
+        private int wins, losses;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        
+        }
+
+        public int Wins
+        {
+            get
+            {
+                return wins;
+            }
+            set
+            {
+                wins = value;
+            }
+        }
+        public int Losses
+        {
+            get
+            {
+                return losses;
+            }
+            set
+            {
+                losses = value;
+            }
+        }
+
+
+    }
+}
---
+++ IST303_Asbury_C_A3/IST303_Asbury_C_A3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST303_Asbury_C_A3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
