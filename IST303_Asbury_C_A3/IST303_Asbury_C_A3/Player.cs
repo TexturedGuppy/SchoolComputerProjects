5e95fc07a06775225939e9b4c794ffc59bace74d
Cameron Asbury <asbury56@marshall.edu>
2016-10-05 08:29:42 -0400

    Almost Finished A3
    Players can still play against themselves.

--- IST303_Asbury_C_A3/IST303_Asbury_C_A3/Player.cs
+++ IST303_Asbury_C_A3/IST303_Asbury_C_A3/Player.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace IST303_Asbury_C_A3
 {
     class Player
     {
-        private string sName;
-        private int iWins; 
-        private int iLosses;
+        //Private member variables
+        private string name;
+        private int wins; 
+        private int losses;
+
+        //Static random to give reliable results
         static Random rand = new Random();
 
         //Enum to track moves of player
@@ -22,6 +25,7 @@ namespace IST303_Asbury_C_A3
         };
 
         //Enum to track wins, loses, and draws
+        //James recommended to me to use two sets of enums
         enum Results
         {
             eWin,
@@ -35,11 +39,11 @@ namespace IST303_Asbury_C_A3
         {
             get
             {
-                return sName;
+                return name;
             }
             set
             {
-                sName = value;
+                name = value;
             }
         
         }
@@ -48,49 +52,128 @@ namespace IST303_Asbury_C_A3
         {
             get
             {
-                return iWins;
+                return wins;
             }
             set
             {
-                iWins = value;
+                wins = value;
             }
         }
         public int Losses
         {
             get
             {
-                return iLosses;
+                return losses;
             }
             set
             {
-                iLosses = value;
+                losses = value;
             }
         }
 
         //Player Constructor, sets wins and losses to 0 and sets the player's name.
         public Player(string name)
         {
-            sName = name;
+            this.name = name;
             Wins = 0;
             Losses = 0;
         }
 
-        public void Players(params string[] playerNames)
-        {
-            List<string> playerList = new List<string>();
-            foreach (string name in playerNames)
-            {
-                playerList.Add(name);
-                Console.WriteLine(name);
-            }
-            Console.WriteLine();
-        }
-
+        
+        /// <summary>
+        /// Two players play against eachother
+        /// Checks to see if they are the same person
+        /// Checks for draws, wins, and losses
+        /// </summary>
+        /// <param name="opponent"></param>
         public void Play(Player opponent)
         {
-            Actions player1 = (Actions)rand.Next(0,3);
-            Actions player2 = (Actions)rand.Next(0, 3);
+            Console.WriteLine("{0} is playing against {1}!", name, opponent.Name);
+            //Sentinel Value for while loop, becomes false once players actually "play" against eachother
+            bool bDraw = true;
+            while (bDraw)
+            {
+                Actions player1 = (Actions)rand.Next(0 , 3);
+                Actions player2 = (Actions)rand.Next(0 , 3);
 
-        }
+                if (name == opponent.Name)
+                {
+                    Console.WriteLine("Can't play yourself.");
+                    
+                }
+                Console.WriteLine("Rock, Paper, Scissors...Shoot!");
+
+                if (player1 != player2)
+                {
+
+                    //Rock
+                    if (player1 == Actions.eRock)
+                    {
+                        if (player2 == Actions.ePaper)
+                        {
+                            Console.WriteLine("{0} wins!",opponent.Name);
+                            losses++;
+                            opponent.Wins++;
+                        }
+                        if (player2 == Actions.eScissors)
+                        {
+                            Console.WriteLine("{0} wins!", name);
+                            wins++;
+                            opponent.Losses++;
+                        }
+
+                    }
+                    //Paper
+                    if (player1 == Actions.ePaper)
+                    {
+                        if (player2 == Actions.eRock)
+                        {
+                            Console.WriteLine("{0} wins!", name);
+                            wins++;
+                            opponent.Losses++;
+                        }
+                        if (player2 == Actions.eScissors)
+                        {
+
+                            Console.WriteLine("{0} wins!",opponent.Name);
+                            losses++;
+                            opponent.Wins++;
+                        }
+                    }
+                    //Scissors
+                    if (player1 == Actions.eScissors)
+                    {
+                        if (player2 == Actions.ePaper)
+                        {
+                            Console.WriteLine("{0} wins!", name);
+                            wins++;
+                            opponent.Losses++;
+                        }
+                        if (player2 == Actions.eRock)
+                        {
+                            Console.WriteLine("{0} wins!",opponent.Name);
+                            losses++;
+                            opponent.Wins++;
+                        }
+                    }
+
+                    bDraw = false;
+                }
+
+                if(player1 == player2)
+                {
+                    Console.WriteLine("Draw...Play Again!");
+                    Thread.Sleep(1000);
+
+                }
+
+            }
+            //Sleep forces program to wait specified number of milleseconds
+            Thread.Sleep(1000); 
+        } 
+        //End Play()
+                
+
+            
     }
 }
--- IST303_Asbury_C_A3/IST303_Asbury_C_A3/Program.cs
+++ IST303_Asbury_C_A3/IST303_Asbury_C_A3/Program.cs
@@ -8,8 +8,67 @@ namespace IST303_Asbury_C_A3
 {
     class Program
     {
+        static Random rand = new Random();
+
+        /// <summary>
+        /// Function takes in unlimited names and adds them into a list, then returns list.
+        /// Had to make static to use it in program
+        /// </summary>
+        /// <param name="playerNames"></param>
+        /// <returns></returns>
+        static List<Player> Players(params string[] playerNames)
+        {
+            List<Player> playerList = new List<Player>();
+            foreach (string name in playerNames)
+            {
+                playerList.Add(new Player(name));
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+            return playerList;
+        }
         static void Main(string[] args)
         {
+            List<Player> players = Players("Cameron", "James", "Matt", "Megaman", "Zero");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0} is playing his 5 randomized rounds!",players[i].Name);
+                for (int j = 0; j < 5; j++)
+                {
+                    players[i].Play(players[rand.Next(0,5)]);
+                }
+                Console.WriteLine();
+            }
+
+            //LINQ for displaying the leaderboard in descending order.
+            //Microsoft MSDN recommends using var instead of IEnumerable<ObjectName> for LINQ
+            var leaderboard = from player in players orderby player.Wins descending select player;
+            Console.WriteLine("LEADERBOARD:");
+            Console.WriteLine("{0,-10} {1,-10} {2,-10}", "NAME","WINS","LOSSES");
+            foreach (Player player in leaderboard)
+            {
+                Console.WriteLine("{0,-10} {1,-10} {2,-10}", player.Name, player.Wins, player.Losses);
+
+            }
+
+            Console.WriteLine();
+
+            //LINQ for displaying players with equal amounts of wins and losses.
+            var equalWinsAndLosses = from player in players where player.Wins == player.Losses select player;
+            Console.WriteLine("PLAYERS WITH EQUAL WINS AND LOSSES:");
+            Console.WriteLine("{0,-10} {1,-10} {2,-10}", "NAME", "WINS", "LOSSES");
+            foreach (Player player in equalWinsAndLosses)
+            {
+                Console.WriteLine("{0,-10} {1,-10} {2,-10}", player.Name, player.Wins, player.Losses);
+
+            }
+
         }
+
+
+
     }
 }
+
+
