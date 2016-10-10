4621551543845eb264fb0d261a8997b91853fd22
Cameron Asbury <asbury56@live.marshall.edu>
2016-10-10 12:02:15 -0400

    Midterm Review

---
+++ MidtermReview/MidtermReview/NinjaTurtle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermReview
+{
+    //structs are value types
+    //structs can't use inheritance
+    //value types are the actual value, not a pointer
+    struct Weapon
+    {
+       public int damage;
+       public int range;
+    }
+    //Classes allow us to define our own data type
+    //classes are reference type
+    //reference types are equivalent to smart pointers in c++
+    class NinjaTurtle
+    {
+        //Constructor - called when we use new to make an object
+        //Dont forget to make public because if you don't you could no longer create new objects of this class type in main.
+        public NinjaTurtle()
+        {
+            name = "Unnamed";
+            weapon.damage = 10;
+            weapon.range = 5;
+        }
+        //(instance or member) variables, attributes, fields
+        private string name;
+        private Weapon weapon;
+
+        //Property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            //can use private set, so member functions can set values but Main cannot
+           set
+            {
+                //logic to control how name gets changed
+                name = value;
+            }
+        }
+        //Equivalent to get block in property
+        public string getName()
+        {
+            return name;
+        }
+        //Equivalent to set block in property
+        public void setName(string Name)
+        {
+            Name = name;
+        }
+
+        //(member) functions, methods, operations
+        //public, private, protected are access specifiers
+        public void Attack(NinjaTurtle other)
+        {
+
+        }
+    }
+}
---
+++ MidtermReview/MidtermReview/Program.cs
@@ -0,0 +1,47 @@
+﻿//System comes from the .NET framework
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermReview
+{
+    class Program
+    {
+        static void Main(string[] args)//Entry Point - Where the program is actually starting
+        {
+            //Console comes from system
+            Console.WriteLine("Hello World");
+            Console.WriteLine("{0, -5}. {1}", 1, "Soda");
+            Console.WriteLine("{0, -5}. {1}", 2, "Coffee");
+            //string str;
+            //str = Console.ReadLine();
+
+            ////Have to convert to Int before you can read the line, because readline returns a string
+            int poop;
+            //poop = Convert.ToInt32(Console.ReadLine());
+
+            //The above will crash if its not int so we can try this below
+            //It will try to parse what the user enters, if it can it will print what they typed
+            //If it can't it just wont try it so the program won't crash.
+            //if (int.TryParse(Console.ReadLine(), out poop))
+            //{
+            //    Console.WriteLine(poop);
+            //}
+
+            //We have to use new with a reference type
+            //to actually create an object
+            NinjaTurtle deCaprio = new NinjaTurtle();
+            deCaprio.Name = "Leonardo";
+            Console.WriteLine(deCaprio.Name);
+
+            NinjaTurtle raphael = deCaprio;
+            Console.WriteLine(raphael.Name);
+            raphael.Name = "Raphael";
+            Console.WriteLine(deCaprio.Name);
+            raphael = new NinjaTurtle();
+
+        }
+    }
+}
