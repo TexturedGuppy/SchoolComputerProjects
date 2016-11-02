149538806d9bdccdf3e8e618a09861ac8db4b5ec
Cameron Asbury <asbury56@live.marshall.edu>
2016-11-02 14:17:38 -0400

    Added Hangman Class

---
+++ IST303_Asbury_C_A5/IST303_Asbury_C_A5/Hangman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IST303_Asbury_C_A5
+{
+    class Hangman
+    {
+        private string word;
+        public Hangman()
+        {
+            word = "default";
+        }
+
+
+    }
+}
