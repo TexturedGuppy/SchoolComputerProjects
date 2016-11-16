aae35166807f0405e320af73b715b102247bc3f2
Cameron Asbury <asbury56@live.marshall.edu>
2016-11-16 11:55:57 -0500

    notes for generics

--- Generics/Generics/Form1.cs
+++ Generics/Generics/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -30,6 +31,29 @@ namespace Generics
             List<string> list = new List<string>();
             List<int> intList = new List<int>();
 
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddLast(5);
+            linkedList.AddLast(32);
+            richTextBox1.AppendText(linkedList.ElementAt(1).ToString());
+
+            Stack<string> stringStack = new Stack<string>();
+            stringStack.Push("hat");
+            stringStack.Push("bat");
+            stringStack.Push("cat");
+            richTextBox1.AppendText("\n\n");
+            richTextBox1.AppendText(stringStack.Peek());//Same as Top in C++
+            stringStack.Pop();
+            richTextBox1.AppendText(stringStack.Peek());
+
+            Queue<string> stringQueue = new Queue<string>();
+            stringQueue.Enqueue("Player 1");
+            stringQueue.Enqueue("Player 2");
+            stringQueue.Enqueue("Player 3");
+            richTextBox1.AppendText("\n\n");
+            richTextBox1.AppendText(stringQueue.Peek());
+            stringQueue.Enqueue(stringQueue.Dequeue());//Makes that player basically walk to the back of the line
+            richTextBox1.AppendText(stringQueue.Peek());
+
             list.Add("bob");
             list.Add("hob");
             list.Add("gob");
@@ -50,7 +74,20 @@ namespace Generics
 
             SmartArray<int> ourIntList = new SmartArray<int>();
             ourIntList.Add(5);
+
+            Hashtable table = new Hashtable();
+            table.Add(0,"Matt");
+            table.Add(3.14, stringQueue);
+
+            Dictionary<string, double> dictionary = new Dictionary<string, double>();
+            dictionary.Add("WV", .06);
+            dictionary.Add("PI", 3.14);
+            dictionary.Add("KY", .08);
+            richTextBox1.AppendText(dictionary["PI"].ToString());
+
         }
+
+        //Generic Function, where T can take any type of data
         void PrintArray<T>(IEnumerable<T> arr)
         {
             foreach(T i in arr)
