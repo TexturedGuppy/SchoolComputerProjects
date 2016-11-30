cb970b4a41b699f450d87d947c0f241fe81fd5a8
Cameron Asbury <asbury56@live.marshall.edu>
2016-11-30 11:54:54 -0500

    Chat Program
    Will broadcast to entire network of anyone running chat program.

---
+++ ChatProgram/ChatProgram/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ChatProgram
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(12, 12);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(300, 246);
+            this.txtChat.TabIndex = 0;
+            this.txtChat.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(443, 355);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 304);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(300, 20);
+            this.txtSend.TabIndex = 2;
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.Location = new System.Drawing.Point(472, 72);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(120, 95);
+            this.lstUsers.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 475);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtChat);
+            this.Name = "frmMain";
+            this.Text = "Chat Program";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.ListBox lstUsers;
+    }
+}
+
---
+++ ChatProgram/ChatProgram/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ChatProgram
+{
+    public partial class frmMain : Form
+    {
+        UdpClient sendingClient;
+        UdpClient receivingClient;
+        int port = 1234;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(txtSend.Text);
+            sendingClient.Send(msg, msg.Length);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+            sendingClient = new UdpClient();
+            sendingClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            sendingClient.Connect(endPoint);
+
+            receivingClient = new UdpClient();
+            receivingClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            endPoint = new IPEndPoint(IPAddress.Any, port);
+            receivingClient.Client.Bind(endPoint);
+            Task.Factory.StartNew(() => Listen());
+        }
+
+        private async void Listen()
+        {
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] buffer = receivingClient.Receive(ref endPoint);
+                string msg = Encoding.ASCII.GetString(buffer);
+                txtChat.AppendText(msg + "\n");
+                await Task.Factory.StartNew(() => Listen());
+            }
+            catch(Exception e)
+            {
+                txtChat.AppendText("Error: " + e.Message + "\n");
+            }
+        }
+    }
+}
